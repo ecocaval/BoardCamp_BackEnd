ee66db31726656ee3b19ac232fc98f03f684149f
erico <ecocaval@hotmail.com>
2023-02-09 05:34:11 -0300

    adding feature: GET /rentals (with JOIN)

--- src/controllers/rentalsController/rentalsController.js
+++ src/controllers/rentalsController/rentalsController.js
@@ -5,7 +5,31 @@ import { getGamePricePerDayById } from "../gamesController/utils/getGamePricePer
 
 export async function getRentals(_, res) {
     try {
-        const rentals = await db.query("SELECT * FROM rentals") //! Do join after
+        const rentals = await db.query(`
+                SELECT
+
+                rentals.id,
+                rentals."customerId",
+                rentals."gameId",
+                rentals."rentDate",
+                rentals."daysRented",
+                rentals."returnDate",
+                rentals."originalPrice",
+                
+                rentals."delayFee",
+                json_build_object(
+                    'id', customers.id,
+                    'name', customers."name"
+                ) AS customer,
+                json_build_object(
+                    'id', games.id,
+                    'name', games."name"
+                ) AS game
+                
+                FROM rentals
+                INNER JOIN customers ON rentals."customerId" = customers.id
+                INNER JOIN games ON rentals."gameId" = games.id;
+            `)
 
         return res.send(rentals.rows)
 
