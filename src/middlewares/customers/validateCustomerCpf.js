cb2461c321e41b5e664083bf1282dc64aa333b12
erico <ecocaval@hotmail.com>
2023-02-10 20:16:48 -0300

    adding fix: fixing validateCustomerCpf()

--- src/middlewares/customers/validateCustomerCpf.js
+++ src/middlewares/customers/validateCustomerCpf.js
@@ -4,11 +4,18 @@ import { db } from "../../config/database.connection.js"
 export async function validateCustomerCpf(req, res, next) {
 
     const { cpf } = structuredClone(req.sanitizedBody)
+    const { id } = structuredClone(req.params)
 
     try {
         const customers = await db.query('SELECT * FROM customers WHERE cpf = $1', [cpf])
 
-        if (customers.rowCount > 0) return res.sendStatus(409)
+        if (customers.rowCount > 0) {
+            if (id) {
+                if(!(customers.rows[0].id == id)) return res.sendStatus(409)
+            } else {
+                return res.sendStatus(409)
+            }
+        }
 
     } catch (err) {
         console.log(err)
--- src/schemas/registerGameSchema.js
+++ src/schemas/registerGameSchema.js
@@ -5,5 +5,5 @@ export const registerGameSchema = Joi.object({
     name: Joi.string().trim().allow("").required(),
     image: Joi.string().trim().uri().required(),
     stockTotal: Joi.number().positive().integer().required(),
-    pricePerDay: Joi.number().required(),
+    pricePerDay: Joi.number().positive().integer().required(),
 })
